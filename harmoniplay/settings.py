924bab1c5973c3410ac8b0e379893bfdbf9aea69
bmuia <belammuia0@gmail.com>
2025-07-21 14:02:36 +0300

    setup harmoniplay authentication

--- harmoniplay/settings.py
+++ harmoniplay/settings.py
@@ -34,6 +34,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000'
+]
+
 
 # Application definition
 
@@ -47,12 +51,14 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'rest_framework_simplejwt',
-     'rest_framework_simplejwt.token_blacklist',
+    'rest_framework_simplejwt.token_blacklist',
+    'corsheaders',
 
     'accounts',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
